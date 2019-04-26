870742cef825c58b8ad73b0a6a05a65602472ffb
Isaac Speed <ies4466@truman.edu>
2019-04-26 16:58:30 -0500

    Working cart pole with RL

--- Model.py
+++ Model.py
@@ -1,41 +1,59 @@
 from keras.models import Sequential
 from keras.layers import Dense, Activation
 from keras import optimizers
+from collections import deque
 import numpy as np
 import random
 
 class Model():
 
     
-    def __init__(self, state_shape, num_actions, learning_rate=0.1, gamma=0.8, batch_size=50):
+    def __init__(self, state_shape, num_actions, learning_rate=0.001, gamma=0.8, batch_size=50, max_replays=2000, starting_epsilon=1.0):
         self.state_shape = state_shape
         self.num_actions = num_actions
         self.learning_rate = learning_rate
         self.gamma = gamma # Reward decay
+        self.epsilon = starting_epsilon
         self.batch_size = batch_size
+        self.max_replays = max_replays
+
     
-        self.memory = []
+        self.memory = deque()
 
         self.model = Sequential()
         self.model.add(Dense(32, input_shape=self.state_shape, activation='relu'))
-        self.model.add(Dense(10, activation='relu'))
+        self.model.add(Dense(24, activation='relu'))
+        self.model.add(Dense(24, activation='relu'))
         self.model.add(Dense(self.num_actions, activation='linear'))
         
         self.model.compile(loss='mse',
                     optimizer=optimizers.Adam(lr=self.learning_rate))
 
     # TODO exploration vs exploitation
-    def predict(self, state):
-        return self.model.predict(state.reshape(1, self.state_shape[0])).flatten()
+    def act(self, state):
+        if random.random() < self.epsilon:
+            return random.randint(0, self.num_actions-1)
+        return np.argmax(self.model.predict(state))
 
     def remember(self, state, action, next_state, reward, done):
+        if len(self.memory) > self.max_replays: self.memory.popleft()
         self.memory.append([state, action, next_state, reward, done])
 
     def replay(self):
-        batch = random.sample(self.memory, self.batch_size)
+        if self.epsilon > 0.01: self.epsilon *= 0.995
+        if len(self.memory) > self.batch_size:
+            batch = random.sample(self.memory, self.batch_size)
+        else:
+            batch = list(self.memory)
+            random.shuffle(batch)
 
         for state, action, next_state, reward, done in batch:
             if done:
-                target = reward
+                discounted_reward = reward
             else:
-                target = reward # TODO im confuse
+                discounted_reward = reward + self.gamma * np.amax(self.model.predict(next_state))
+
+            target = self.model.predict(state)
+            target[0][action] = discounted_reward
+
+            self.model.fit(state, target, verbose=0)
--- RL_Experiment.py
+++ RL_Experiment.py
@@ -1,22 +1,27 @@
 import gym
 from Model import Model
 
-NUM_EPISODES = 100
+NUM_EPISODES = 1000
 
-env = gym.make('BipedalWalker-v2')
-state = env.reset()
+env = gym.make('CartPole-v0')
+state_size = env.observation_space.shape[0]
+state = env.reset().reshape(1, state_size)
 
-model = Model(state_shape = env.observation_space.shape, num_actions = env.action_space.shape[0])
+model = Model(state_shape = env.observation_space.shape, num_actions = env.action_space.n)
 
 for e in range(NUM_EPISODES):
-    for _ in range(500):
-        env.render()
-        action = model.predict(state)
+    state = env.reset().reshape(1, state_size)
+    for t in range(500):
+        if e > 200 and e % 5 == 0: env.render()
+        action = model.act(state)
         old_state = state
         state, reward, done, info = env.step(action)
+        state = state.reshape(1, state_size)
         model.remember(old_state, action, state, reward, done)
 
         if done:
             break
         # env.step(env.action_space.sample()) # take a random action
+    model.replay()
+    print(f"Episode {e} lasted {t} frames. epsilon: {model.epsilon}.")
 env.close()
